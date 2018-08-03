186fc211f74b93bed6239f5f925a2f073783ef83
shaharke <shahar@bigpanda.io>
2018-08-03 19:18:58 +0300

    Authneticate against DynamoDB

--- server/auth.js
+++ server/auth.js
@@ -18,3 +18,35 @@ exports.authenticate = async function (client, event) {
   }
   
 }
+
+exports.authenticateDynamo = async function (ddb, event) {
+  try {
+    const token = event.queryStringParameters.id_token;
+    if (!token) {
+      throw new AuthError('Token must be present');
+    }
+    const userClaims = await decode(token);
+    const email = userClaims.email;
+
+    const query = {
+      TableName: "members",
+      Key: {
+        email
+      }
+    }
+
+    return new Promise((resolve, reject) => {
+      ddb.get(query, (err, member) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(member);
+      })
+    })
+  } catch (e) {
+    throw new AuthError(e.message);
+  }
+  
+}
+
