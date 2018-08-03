e2a0a7a4415354ed0eed82bee76c18be1c8785ae
shaharke <shahar@bigpanda.io>
2018-08-03 19:18:58 +0300

    Authneticate against DynamoDB

--- server/auth.js
+++ server/auth.js
@@ -1,5 +1,6 @@
 const { decode } = require('decode-verify-jwt');
 const { AuthError } = require('errors');
+const { get: getMember} = require('./dao/members');
 
 exports.authenticate = async function (client, event) {
   try {
@@ -18,3 +19,23 @@ exports.authenticate = async function (client, event) {
   }
   
 }
+
+exports.authenticateDynamo = async function (ddb, event) {
+  try {
+    const token = event.queryStringParameters.id_token;
+    if (!token) {
+      throw new AuthError('Token must be present');
+    }
+    const userClaims = await decode(token);
+    const email = userClaims.email;
+    const member = await getMember(ddb, email);
+    if (!member) {
+      throw new AuthError(`${email} is not a member`);
+    }
+    return member
+  } catch (e) {
+    throw new AuthError(e.message);
+  }
+  
+}
+
